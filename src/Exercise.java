f822fb213c84721100e77bac077bdd394a1c757f
MattBrown <litteriver1337@gmail.com>
2015-10-15 09:57:36 -0400

    Added actual loop!

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by MattBrown on 10/15/15.
  */
 public class Exercise {
+    public static void main(String[] args) {
+
+
+        System.out.println("Would you like to say Hello? [y/n]");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        while (!name.equals(("n"))) {
+            if (name.equals("y")) {
+                System.out.println("Hello!");
+                System.exit(0);
+            } else {
+
+            }
+        }
+    }
 }
